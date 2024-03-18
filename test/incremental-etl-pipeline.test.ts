659a13b7b442b99f1d0aba0d4c0c53fecf8a6787
Kinyanjui Karanja <ekkinyanjui@gmail.com>
2024-03-18 16:46:30 +0300

    Initial commit

---
+++ test/incremental-etl-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IncrementalEtlPipeline from '../lib/incremental-etl-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/incremental-etl-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IncrementalEtlPipeline.IncrementalEtlPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
