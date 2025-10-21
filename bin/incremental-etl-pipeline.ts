9655a2bf7b141ead6a08d566d0af373bd6bbb1ec
Elijah Karanja <ekkinyanjui@gmail.com>
2025-10-21 18:49:30 +0300

    Finish producer and consumer

--- bin/incremental-etl-pipeline.ts
+++ bin/incremental-etl-pipeline.ts
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { IncrementalEtlPipelineStack } from '../lib/incremental-etl-pipeline-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { KinesisStack } from "../lib/kinesis-stack";
+import { DataProducerStack } from "../lib/data-producer-stack";
+import { DataConsumerStack } from "../lib/data-consumer-stack";
+
+const ALPHA_VANTAGE_KEY: string = process.env.ALPHA_VANTAGE_KEY || "";
+const INTRADAY_STREAM_NAME =
+  process.env.INTRADAY_STREAM_NAME || "intraday-analytics-stream";
+const DYNAMO_TABLE_NAME = process.env.DYNAMO_TABLE_NAME || "DataConsumerTable";
 
 const app = new cdk.App();
-new IncrementalEtlPipelineStack(app, 'IncrementalEtlPipelineStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new KinesisStack(app, "KinesisStack", {
+  streamName: INTRADAY_STREAM_NAME,
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+new DataProducerStack(app, "DataProducerStack", {
+  streamName: INTRADAY_STREAM_NAME,
+  alphaVantageKey: ALPHA_VANTAGE_KEY,
+});
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new DataConsumerStack(app, "DataConsumerStack", {
+  streamName: INTRADAY_STREAM_NAME,
+  tableName: DYNAMO_TABLE_NAME,
 });
