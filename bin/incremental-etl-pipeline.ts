5de0ace133c4a205569a6074eba28fd0b22a78b5
Elijah Karanja <ekkinyanjui@gmail.com>
2025-10-28 19:04:51 +0300

    Added glue job

--- bin/incremental-etl-pipeline.ts
+++ bin/incremental-etl-pipeline.ts
@@ -4,11 +4,16 @@ import * as cdk from "aws-cdk-lib";
 import { KinesisStack } from "../lib/kinesis-stack";
 import { DataProducerStack } from "../lib/data-producer-stack";
 import { DataConsumerStack } from "../lib/data-consumer-stack";
+import { GlueSMAStack } from "../lib/glue-sma-stack";
 
 const ALPHA_VANTAGE_KEY: string = process.env.ALPHA_VANTAGE_KEY || "";
 const INTRADAY_STREAM_NAME =
   process.env.INTRADAY_STREAM_NAME || "intraday-analytics-stream";
 const DYNAMO_TABLE_NAME = process.env.DYNAMO_TABLE_NAME || "DataConsumerTable";
+const GLUE_DATABASE_NAME = process.env.GLUE_DATABASE_NAME || "crypto_sma_db";
+const GLUE_TABLE_NAME = process.env.GLUE_TABLE_NAME || "crypto_sma_table";
+const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME || "crypto-etl-data-bucket";
+const S3_PREFIX = process.env.S3_PREFIX || "processed-data/";
 
 const app = new cdk.App();
 
@@ -24,4 +29,12 @@ new DataProducerStack(app, "DataProducerStack", {
 new DataConsumerStack(app, "DataConsumerStack", {
   streamName: INTRADAY_STREAM_NAME,
   tableName: DYNAMO_TABLE_NAME,
+});
+
+new GlueSMAStack(app, "GlueSMAStack", {
+  databaseName: GLUE_DATABASE_NAME,
+  tableName: GLUE_TABLE_NAME,
+  s3BucketName: S3_BUCKET_NAME,
+  s3Prefix: S3_PREFIX,
+  dynamodbTableName: DYNAMO_TABLE_NAME,
 });
--- build_layers.py
+++ build_layers.py
@@ -7,5 +7,10 @@ layers = os.listdir(directory)
 for layer in layers:
     layer_path = os.path.join(directory, layer)
     if os.path.isdir(layer_path):
+        # Check if requirements.txt exists in the layer directory
+        requirements_path = os.path.join(layer_path, 'requirements.txt')
+        if not os.path.isfile(requirements_path):
+            print(f'Skipping layer {layer} as no requirements.txt found.')
+            continue
         print(f'Building layer: {layer}')
         os.system(f'cd {layer_path} && mkdir -p python && pip install -r requirements.txt -t python')
